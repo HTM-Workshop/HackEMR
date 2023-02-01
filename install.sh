879be07742331603e572266889faac8a47809379
Kevin Williams <kevinwilliams.gwcd@gmail.com>
2023-02-01 14:51:42 -0500

    Added install script for Debian

--- install.sh
+++ install.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
+# check if we're running as root
 if [ "$EUID" -ne 0 ]
   then echo "This script must be run with root permissions."
   exit
 fi
 
-
+# install dependencies
+echo "-- Installing dependencies..."
 apt update
 apt -y install php php-mysql apache2 acl mariadb-server openssh-server vim htop tree zip rsync
+
+# create working directory if it doesn't exist for some reason
+mkdir -p /var/www/html/
+
+# copy files to working directory and create test file structure
+echo "-- Copying files..."
+rsync -aPvr * /var/www/html/
+
+echo "-- Setting up working directory..."
+chown -R www-data /var/www 
+chgrp -R www-data /var/www 
 cd /var/www/html/
 for i in {1..5}
 do
     mkdir -p patient_files/$num
 done
 
+echo "-- Configuring SQL server..."
+cd sql
+chmod +x runasroot.sh
+./runasroot.sh
--- sql/runasroot.sh
+++ sql/runasroot.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-apt update
-apt -y install mariadb-server
 mariadb < db_init.sql
 mariadb < auth_db.sql
 mariadb < patient.sql
-mariadb < tips.db
+mariadb < tips.sql
